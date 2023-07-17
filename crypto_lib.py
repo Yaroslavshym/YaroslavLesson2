cb641e3ac7e38287dfc4355ac72e10fc4f137b09
yaroslav shym <yaroslavshym@MacBook-Air-yaroslav.local>
2023-07-17 20:40:14 +0100

    test_passlib_data()

--- crypto_lib.py
+++ crypto_lib.py
@@ -1,7 +1,26 @@
 import hashlib
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+
 def encode_md5(value:str) -> str:
     resoult = hashlib.md5(value.encode()).hexdigest()
     return resoult
 
 
-print(encode_md5('as'))
+def get_password(password: str) -> str:
+    resoult = pwd_context.hash(password)
+    return resoult
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    result = pwd_context.verify(plain_password, hashed_password)
+    return result
+
+res =  get_password('1')
+print(res)
+h = '$2b$12$.tOrd0AJEK6bmj7379HVVuQto3HFK32pohg9Sx60Nog0IzjUsJHKm'
+print(verify_password('1', h))
